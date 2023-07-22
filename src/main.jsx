5a624d901c18f9f61fd2f3a6dd4a113dd292bf34
0rigin-c0de <shunnu807@gmail.com>
2023-07-22 15:23:32 +0530

    initial-commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
